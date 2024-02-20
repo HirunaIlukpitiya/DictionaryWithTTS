1f055b17ca9173b31d8d8a9937efb2f60aeec497
HirunaIlukpitiya <33183435+HirunaIlukpitiya@users.noreply.github.com>
2024-02-21 00:55:20 +0530

    flex

--- src/views/home.vue
+++ src/views/home.vue
@@ -21,46 +21,107 @@
         </div>
       </div>
     </form>
-    <div v-if="response">
+    <div v-if="response" class="marker:text-blue-600">
       <h1 class="font-sans text-3xl font-semibold text-blue-800">Results</h1>
       <br />
       <ol>
-      ---------------------------------------
-        <div class="flex animate-pulse">
-  <div class="flex-shrink-0">
-    <span class="size-12 block bg-gray-200 rounded-full dark:bg-gray-700"></span>
-  </div>
+        <!-- start of skeleton -->
+        <div class="flex animate-pulse" v-if="this.isLoading">
+          <div class="flex-shrink-0">
+            <span
+              class="size-12 block bg-gray-200 rounded-full dark:bg-gray-700"
+            ></span>
+          </div>
 
-  <div class="ms-4 mt-2 w-full">
-    <h3 class="h-4 bg-gray-200 rounded-full dark:bg-gray-700" style="width: 40%;"></h3>
+          <div class="ms-4 mt-2 w-full">
+            <h3
+              class="h-4 bg-gray-200 rounded-full dark:bg-gray-700"
+              style="width: 40%"
+            ></h3>
 
-    <ul class="mt-5 space-y-3">
-      <li class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"></li>
-      <li class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"></li>
-      <li class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"></li>
-      <li class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"></li>
-    </ul>
-  </div>
-</div>
-------------------------------------
-        <li v-if="this.dataArrayNoun.length != 0" class="italic font-semibold">
+            <ul class="mt-5 space-y-3">
+              <li
+                class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"
+              ></li>
+              <li
+                class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"
+              ></li>
+              <li
+                class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"
+              ></li>
+              <li
+                class="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700"
+              ></li>
+            </ul>
+          </div>
+        </div>
+        <!-- end of the skeleton -->
+        <li v-if="this.dataArrayNoun.length != 0" class="italic font-semibold text-2xl text-blue-700">
           noun :
           <div v-for="data in this.dataArrayNoun">
             <ul>
-              <li class="not-italic font-normal list-disc text-justify">
-                {{ data }}<button @click="defTTS(data)">🔊</button>
+              <div class="flex justify-between">
+              <li class="not-italic font-normal list-disc text-justify text-xl text-black">
+                {{ data }}
               </li>
+              <button @click="defTTS(data)" class="text-3xl hover:scale-125">🔊</button>
+            </div>
             </ul>
           </div>
         </li>
         <br />
-        <li v-if="this.dataArrayVerb.length != 0" class="italic font-semibold">
+        <li v-if="this.dataArrayVerb.length != 0" class="italic font-semibold text-2xl text-blue-700">
           verb :
           <div v-for="data in this.dataArrayVerb">
             <ul>
-              <li class="not-italic font-normal list-disc text-justify">
-                {{ data }}<button @click="defTTS(data)">🔊</button>
+              <div class="flex justify-between">
+              <li class="not-italic font-normal list-disc text-justify text-xl text-black">
+                {{ data }}
               </li>
+              <button @click="defTTS(data)" class="text-3xl hover:scale-125">🔊</button>
+            </div>
+            </ul>
+          </div>
+        </li>
+        <br>
+        <li v-if="this.dataArrayAdjectives.length != 0" class="italic font-semibold text-blue-700 text-2xl">
+          adjective :
+          <div v-for="data in this.dataArrayAdjectives">
+            <ul>
+              <div class="flex justify-between">
+              <li class="not-italic font-normal list-disc text-justify text-xl text-black">
+                {{ data }}
+              </li>
+              <button @click="defTTS(data)" class="text-3xl hover:scale-125">🔊</button>
+            </div>
+            </ul>
+          </div>
+        </li>
+        <br>
+        <li v-if="this.dataArrayAdverb.length != 0" class="italic font-semibold text-2xl text-blue-700">
+          adverb :
+          <div v-for="data in this.dataArrayAdverb">
+            <ul>
+              <div class="flex justify-between">
+              <li class="not-italic font-normal list-disc text-justify text-xl text-black">
+                {{ data }}
+              </li>
+              <button @click="defTTS(data)" class="text-3xl hover:scale-125">🔊</button>
+            </div>
+            </ul>
+          </div>
+        </li>
+        <br>
+        <li v-if="this.dataArrayInterjection.length != 0" class="italic font-semibold text-2xl text-blue-700">
+          interjection :
+          <div v-for="data in this.dataArrayInterjection">
+            <ul>
+            <div class="flex justify-between">
+              <li class="not-italic font-normal list-disc text-justify text-xl text-black">
+                {{ data }}
+              </li>
+              <button @click="defTTS(data)" class="text-3xl hover:scale-125">🔊</button>
+            </div>
             </ul>
           </div>
         </li>
@@ -70,8 +131,8 @@
 </template>
 <script>
 import axios from "axios";
-import { toast } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 var inputWord = "";
 var APIlink = "";
 var errorMessage = "";
@@ -80,6 +141,7 @@ export default {
   data() {
     return {
       word: "",
+      isLoading: false,
       response: "",
       dataArray: [],
       refArray: [],
@@ -87,6 +149,7 @@ export default {
       dataArrayVerb: [],
       dataArrayAdverb: [],
       dataArrayAdjectives: [],
+      dataArrayInterjection: [],
       synonyms: [],
       phonetic: "",
     };
@@ -100,10 +163,12 @@ export default {
       this.dataArrayAdverb = [];
       this.dataArrayAdjectives = [];
       this.synonyms = [];
+      this.dataArrayInterjection = [];
       inputWord = this.word;
+      this.isLoading = true;
       APIlink = `https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`;
-      console.log(inputWord);
-      console.log(APIlink);
+      //console.log(inputWord);
+      //console.log(APIlink);
       axios
         .get(APIlink, {})
         .then((response) => {
@@ -121,16 +186,16 @@ export default {
           for (let i = 0; i < dataArrayLength; i++) {
             if (this.dataArray[i].partOfSpeech === "noun") {
               let arrayElementLength = this.dataArray[i].definitions.length;
-              console.log("arrayElementLength :", arrayElementLength);
+              // console.log("arrayElementLength :", arrayElementLength);
               this.refArray = this.dataArray[i].definitions;
               // console.log("refArray : ", this.refArray);
               for (let r = 0; r < arrayElementLength; r++) {
                 this.dataArrayNoun.push(this.refArray[r].definition);
               }
               this.refArray = [];
-            } else {
+            } else if(this.dataArray[i].partOfSpeech === "verb"){
               let arrayElementLength = this.dataArray[i].definitions.length;
-              console.log("arrayElementLength :", arrayElementLength);
+              // console.log("arrayElementLength :", arrayElementLength);
               this.refArray = this.dataArray[i].definitions;
               // console.log("refArray : ", this.refArray);
               for (let r = 0; r < arrayElementLength; r++) {
@@ -138,23 +203,58 @@ export default {
               }
               this.refArray = [];
             }
+            else if(this.dataArray[i].partOfSpeech === "adjective"){
+              let arrayElementLength = this.dataArray[i].definitions.length;
+              // console.log("arrayElementLength :", arrayElementLength);
+              this.refArray = this.dataArray[i].definitions;
+              // console.log("refArray : ", this.refArray);
+              for (let r = 0; r < arrayElementLength; r++) {
+                this.dataArrayAdjectives.push(this.refArray[r].definition);
+              }
+              this.refArray = [];
+
+            }
+            else if(this.dataArray[i].partOfSpeech === "adverb"){
+              let arrayElementLength = this.dataArray[i].definitions.length;
+              // console.log("arrayElementLength :", arrayElementLength);
+              this.refArray = this.dataArray[i].definitions;
+              // console.log("refArray : ", this.refArray);
+              for (let r = 0; r < arrayElementLength; r++) {
+                this.dataArrayAdverb.push(this.refArray[r].definition);
+              }
+              this.refArray = [];
+
+            }
+            else if(this.dataArray[i].partOfSpeech === "interjection"){
+              let arrayElementLength = this.dataArray[i].definitions.length;
+              // console.log("arrayElementLength :", arrayElementLength);
+              this.refArray = this.dataArray[i].definitions;
+              // console.log("refArray : ", this.refArray);
+              for (let r = 0; r < arrayElementLength; r++) {
+                this.dataArrayInterjection.push(this.refArray[r].definition);
+              }
+              this.refArray = [];
+
+            }
           }
-          console.log("dataArrayLength :", dataArrayLength);
-          console.log("dataArray : ", this.dataArray);
-          console.log("dataArrayNoun : ", this.dataArrayNoun);
-          console.log("dataArrayVerb : ", this.dataArrayVerb);
+          // console.log("dataArrayLength :", dataArrayLength);
+          // console.log("dataArray : ", this.dataArray);
+          // console.log("dataArrayNoun : ", this.dataArrayNoun);
+          // console.log("dataArrayVerb : ", this.dataArrayVerb);
+
+          this.isLoading = false;
         })
         .catch((error) => {
           console.log(error);
-          if (error.response.status == 404){
-            errorMessage = "No Definitions Found"
+          if (error.response.status == 404) {
+            errorMessage = "No Definitions Found";
           }
-          this.notifyError()
+          this.notifyError();
         });
     },
     defTTS(data) {
       const APIKey = "70f421f96b8443f8bf44b25b3717fbab";
-      const APIlinkTts = `https://api.voicerss.org/?key=${APIKey}&hl=en-us&b64=true&c=MP3&src=${data}`;
+      const APIlinkTts = `https://api.voicerss.org/?key=${APIKey}&hl=en-gb&v=Nancy&b64=true&f=48khz_16bit_stereo&c=MP3&src=${data}`;
 
       axios
         .get(APIlinkTts)
@@ -178,14 +278,15 @@ export default {
           console.error("Error fetching TTS:", error);
         });
     },
-    notifyError(){
+    notifyError() {
       toast.error(`${errorMessage}!`, {
         autoClose: 2000,
         position: toast.POSITION.TOP_CENTER,
         transition: toast.TRANSITIONS.BOUNCE,
       });
+      this.isLoading = false;
+    },
   },
-}
 };
 </script>
 <style></style>
